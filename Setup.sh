41a57ff592deb27970ffde2d72540560d304125e
mdulog <madulog@gmail.com>
2023-04-14 08:34:25 -0700

    Edited script to add git config

--- Setup.sh
+++ Setup.sh
@@ -1,2 +1,7 @@
+# Download Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Apps from homebrew
 brew bundle
+# git username and password
+git config --global user.name "mdulog"
+git config --global user.email "madulog@gmail.com"
